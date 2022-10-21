41b467789c50e8abd5b13e90588756844bba8ca2
g-gar <g.gar.fern@hotmail.com>
2022-10-21 15:41:34 +0200

    refactor modules

---
+++ management/domain/src/main/java/com/ggar/rayz/management/core/domain/model/Account.java
@@ -0,0 +1,23 @@
+package com.ggar.rayz.management.core.domain.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.OffsetDateTime;
+
+@Data
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@With
+public class Account {
+
+	AccountId id;
+	Username username;
+	Email email;
+	PasswordHash passwordHash;
+	OffsetDateTime created;
+	AccountType role;
+	AccountState state;
+
+}
---
+++ management/domain/src/main/java/com/ggar/rayz/management/core/domain/model/AccountId.java
@@ -0,0 +1,17 @@
+package com.ggar.rayz.management.core.domain.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@With
+public class AccountId {
+
+	UUID value;
+
+}
